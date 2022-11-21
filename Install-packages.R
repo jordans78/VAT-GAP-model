5078f080acaf8ca7a492ab2a15360c914c1cd49d
jordans78 <jordan.simonov78@gmail.com>
2022-11-21 21:30:12 +0100

    Improving dashboard

--- Install-packages.R
+++ Install-packages.R
@@ -7,7 +7,8 @@ install.packages(packageurl, repos=NULL, type="source")
 
 list.of.packages <- c("DataEditR", "data.table","tidyr","readxl","dplyr","reshape2","rccmisc","xlsx",
                       "flexdashboard","ggplot2","plotly","data.table","tidyverse","knitr","DT","rpivotTable",
-                      "forecast","openintro","highcharter","ggvis","DataCombine","viridisLite","readxl","reshape2","orca","sass","vctrs","markdown")
+                      "forecast","openintro","highcharter","ggvis","DataCombine","viridisLite","readxl","reshape2","orca","sass","vctrs","markdown",
+                      "sm","ks")
 
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
