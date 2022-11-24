d135427fd930c332d6a8346d46901287d6c8f88f
jordans78 <jordan.simonov78@gmail.com>
2022-11-24 19:04:22 +0100

    Improving charts

--- Install-packages.R
+++ Install-packages.R
@@ -8,7 +8,7 @@ install.packages(packageurl, repos=NULL, type="source")
 list.of.packages <- c("DataEditR", "data.table","tidyr","readxl","dplyr","reshape2","rccmisc","xlsx",
                       "flexdashboard","ggplot2","plotly","data.table","tidyverse","knitr","DT","rpivotTable",
                       "forecast","openintro","highcharter","ggvis","DataCombine","viridisLite","readxl","reshape2","orca","sass","vctrs","markdown",
-                      "sm","ks","rlang")
+                      "sm","ks","rlang","shades")
 
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
